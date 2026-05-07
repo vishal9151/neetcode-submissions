bbb510586fc13f7a9ea16f700f58301202417a6c
Vishal Mangla <84173784+vishal9151@users.noreply.github.com>
2026-05-08 00:18:16 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        unordered_map<string, vector<string>> mp;
+
+        for(string s : strs)
+        {
+            string temp = s;
+
+            sort(temp.begin(), temp.end());
+
+            mp[temp].push_back(s);
+        }
+
+        vector<vector<string>> result;
+
+        for(auto it : mp)
+        {
+            result.push_back(it.second);
+        }
+
+        return result;
+    }
+};
